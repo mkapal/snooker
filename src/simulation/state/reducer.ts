171e73fe6177032bdf84f502feaddc387d140c38
Martin Kapal <flamecze@gmail.com>
2022-08-20 19:17:42 +0200

    Process all balls in reducer

--- src/simulation/state/reducer.ts
+++ src/simulation/state/reducer.ts
@@ -18,8 +18,8 @@ const initialGameState: GameState = {
         y: 200,
       },
       velocity: {
-        x: 0.4,
-        y: 0.3,
+        x: 0.3,
+        y: 0.2,
       },
     },
     {
@@ -27,12 +27,12 @@ const initialGameState: GameState = {
       isCueBall: false,
       color: 'red',
       position: {
-        x: 380,
-        y: 430,
+        x: 480,
+        y: 530,
       },
       velocity: {
-        x: 0,
-        y: 0,
+        x: -0.1,
+        y: -0.1,
       },
     },
   ],
@@ -46,76 +46,69 @@ export function gameStateReducer(
 ): GameState {
   switch (action.type) {
     case 'SIMULATION.STEP': {
-      const { id, position, velocity } = state.balls[0];
-      const {
-        timeDifference,
-        canvasProps: { ballRadius, canvasHeight, canvasWidth },
-      } = action.payload;
+      const updatedBalls = state.balls.map(ball => {
+        const { position, velocity } = ball;
+        const {
+          timeDifference,
+          canvasProps: { ballRadius, canvasHeight, canvasWidth },
+        } = action.payload;
 
-      const newPosition: Coordinates = {
-        x: position.x + timeDifference * velocity.x,
-        y: position.y + timeDifference * velocity.y,
-      };
-      const newVelocity: Coordinates = {
-        x: velocity.x,
-        y: velocity.y,
-      };
-
-      // Check for table boundaries (left and right)
-      if (
-        newPosition.x < ballRadius ||
-        newPosition.x >= canvasWidth - ballRadius
-      ) {
-        newVelocity.x = -velocity.x;
-      }
-
-      // Check for table boundaries (up and down)
-      if (
-        newPosition.y < ballRadius ||
-        newPosition.y >= canvasHeight - ballRadius
-      ) {
-        newVelocity.y = -velocity.y;
-      }
-
-      // Update ball acceleration
-      newVelocity.x *= BALL_ACCELERATION_COEFFICIENT;
-      newVelocity.y *= BALL_ACCELERATION_COEFFICIENT;
-
-      // Round very small speed values to 0
-      if (Math.abs(newVelocity.x) < ZERO_VELOCITY_THRESHOLD) {
-        newVelocity.x = 0;
-      }
-
-      if (Math.abs(newVelocity.y) < ZERO_VELOCITY_THRESHOLD) {
-        newVelocity.y = 0;
-      }
-
-      if (Math.abs(newVelocity.x) > MAX_VELOCITY) {
-        newVelocity.x = MAX_VELOCITY;
-      }
-
-      if (Math.abs(newVelocity.y) > MAX_VELOCITY) {
-        newVelocity.y = MAX_VELOCITY;
-      }
-
-      const ball = state.balls.find(ball => ball.id === id);
-
-      if (!ball) {
-        return {
-          ...state,
+        const newPosition: Coordinates = {
+          x: position.x + timeDifference * velocity.x,
+          y: position.y + timeDifference * velocity.y,
         };
-      }
+        const newVelocity: Coordinates = {
+          x: velocity.x,
+          y: velocity.y,
+        };
+
+        // Check for table boundaries (left and right)
+        if (
+          newPosition.x < ballRadius ||
+          newPosition.x >= canvasWidth - ballRadius
+        ) {
+          newVelocity.x = -velocity.x;
+        }
+
+        // Check for table boundaries (up and down)
+        if (
+          newPosition.y < ballRadius ||
+          newPosition.y >= canvasHeight - ballRadius
+        ) {
+          newVelocity.y = -velocity.y;
+        }
+
+        // Update ball acceleration
+        newVelocity.x *= BALL_ACCELERATION_COEFFICIENT;
+        newVelocity.y *= BALL_ACCELERATION_COEFFICIENT;
+
+        // Round very small speed values to 0
+        if (Math.abs(newVelocity.x) < ZERO_VELOCITY_THRESHOLD) {
+          newVelocity.x = 0;
+        }
+
+        if (Math.abs(newVelocity.y) < ZERO_VELOCITY_THRESHOLD) {
+          newVelocity.y = 0;
+        }
+
+        if (Math.abs(newVelocity.x) > MAX_VELOCITY) {
+          newVelocity.x = MAX_VELOCITY;
+        }
+
+        if (Math.abs(newVelocity.y) > MAX_VELOCITY) {
+          newVelocity.y = MAX_VELOCITY;
+        }
+
+        return {
+          ...ball,
+          position: newPosition,
+          velocity: newVelocity,
+        };
+      });
 
       return {
         ...state,
-        balls: [
-          ...state.balls.filter(ball => ball.id !== id),
-          {
-            ...ball,
-            position: newPosition,
-            velocity: newVelocity,
-          },
-        ],
+        balls: updatedBalls,
       };
     }
 
@@ -126,6 +119,8 @@ export function gameStateReducer(
         return state;
       }
 
+      const { dragVector } = action.payload;
+
       return {
         ...state,
         balls: [
@@ -133,8 +128,8 @@ export function gameStateReducer(
           {
             ...cueBall,
             velocity: {
-              x: Math.min(action.payload.dragVector.x / 100, MAX_VELOCITY),
-              y: Math.min(action.payload.dragVector.y / 100, MAX_VELOCITY),
+              x: Math.min(dragVector.x / 100, MAX_VELOCITY),
+              y: Math.min(dragVector.y / 100, MAX_VELOCITY),
             },
           },
         ],
